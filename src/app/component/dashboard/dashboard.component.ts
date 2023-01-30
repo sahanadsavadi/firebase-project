a047dcd4a1fcb042aa60143e71932593a87875d9
s.savadi <savadisahand@gmail.com>
2023-01-30 12:45:18 +0330

    feat: crud student in dashboard component
    Signed-off-by: s.savadi <savadisahand@gmail.com>

--- src/app/component/dashboard/dashboard.component.ts
+++ src/app/component/dashboard/dashboard.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Student } from 'src/app/model/student';
 import { AuthService } from 'src/app/shared/auth.service';
+import { DataService } from 'src/app/shared/data.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,11 +9,52 @@ import { AuthService } from 'src/app/shared/auth.service';
   styleUrls: ['./dashboard.component.scss'],
 })
 export class DashboardComponent implements OnInit {
+  studenstList: Student[] = [];
+  id: string = '';
+  first_name: string = '';
+  last_name: string = '';
+  email: string = '';
+  mobile: string = '';
 
-  constructor(private auth: AuthService) {}
+  constructor(private auth: AuthService, private data: DataService) {}
 
   ngOnInit() {}
+  
+
   LogOut() {
     this.auth.logout();
+  }
+
+  getAllStudent() {
+    this.data.grtAllStudent().subscribe(
+      (res) => {
+        this.studenstList = res.map((e: any) => {
+          const data = e.payload.doc.data();
+          data.id = e.payload.doc.id;
+          return data;
+        });
+      },
+      (error) => {
+        alert('Error While fetching student data');
+      }
+    );
+  }
+
+  addStudent() {}
+
+  updateStudent() {}
+
+  deleteStudent(student: Student) {
+    if (
+      window.confirm(
+        'Are you sure you want to delete' +
+          student.first_name +
+          ' ' +
+          student.last_name +
+          '  ?'
+      )
+    ) {
+      this.data.deletStudent(student);
+    }
   }
 }
